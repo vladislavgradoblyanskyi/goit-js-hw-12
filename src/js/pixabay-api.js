a5e56ad7bea4f5d31ac281679ac444f277cfd1ad
vladislavgradoblyanskyi <vladislavgradoblyanskyi@gmail.com>
2026-02-22 22:09:41 +0100

    push1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = 'https://pixabay.com/api/';
 const api_key = '54662429-d0583973866d02e67b13f9c1b';
 
-export function getImagesByQuery(query,page=1) {
+export async function getImagesByQuery(query,page=1) {
   const params = {
     key: api_key,
     q: query,
@@ -13,13 +13,11 @@ export function getImagesByQuery(query,page=1) {
     per_page: 15,
     page,
   };
-
-  return axios.get(api, { params })
-    .then((res) =>{
-      return res.data;
-    })
-    .catch((error) => {
-      console.log(error);
-      
-    });
+  try{
+       const res = await axios.get(api, { params });
+            return res.data;
+  }
+  catch(error){
+    console.log(error);
+  }
 }
